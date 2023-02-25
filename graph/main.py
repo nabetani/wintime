d1f001db1b5d1ddada4345c6fa2bdd27f63ff027
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-02-25 23:59:53 +0900

    グラフ作るスクリプトと、データ

--- graph/main.py
+++ graph/main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-B=[	0.37, 0.42, 0.49, 0.56, 0.65, 0.75, 0.87, 1.00, 1.15, 1.33, 1.54, 1.78,
-    2.05, 2.37, 2.74, 3.16, 3.65, 4.22, 4.87, 5.62, 6.49, 7.50, 8.66, 10.00,
-    11.55, 13.34, 15.40, 17.78, 20.54, 23.71]
+B=[
+		0.12, 0.13, 0.15, 0.18, 0.21, 0.24, 0.27, 0.32,
+		0.37, 0.42, 0.49, 0.56, 0.65, 0.75, 0.87, 1.00, 1.15, 1.33, 1.54, 1.78,
+		2.05, 2.37, 2.74, 3.16, 3.65, 4.22, 4.87, 5.62, 6.49, 7.50, 8.66, 10.00,
+		11.55, 13.34, 15.40, 17.78, 20.54, 23.71,]
 
 def main(fn):
     df = pd.read_csv(fn, header=None,index_col=0)
@@ -13,11 +15,11 @@ def main(fn):
         in zip([""]+B, B+[""]) 
     ]
     plt.style.use("ggplot")
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(15, 18))
-    for i in range(4):
-        ax=axes[i%2, i//2]
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(15, 27))
+    for i in range(6):
+        ax=axes[i//2, i%2]
         df.iloc[i,:].plot(ax=ax, legend=False, kind="barh", width=0.9)
-        ax.set_yticks(list(range(len(ticks))),ticks, fontsize=20, fontname='Menlo')
+        ax.set_yticks(list(range(len(ticks))),ticks, fontsize=16, fontname='Menlo')
         ax.set_title(df.index[i], fontsize=40)
         ax.set_xlim(0,3500)
     plt.tight_layout()
